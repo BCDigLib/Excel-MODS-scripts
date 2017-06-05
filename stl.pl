a8c1b6773107972a0ed5c124dcac4267a0c31c55
Adam Jazairi <adamjazairi@gmail.com>
2017-06-05 16:05:26 -0400

    Fix bad NTLTD schema location

--- stl.pl
+++ stl.pl
@@ -11,28 +11,28 @@ use Data::Dumper;
 
 main();
 
-sub main 
+sub main
 {
 
 	my ($title, $authors, $advisors, $year, $filename);
 
 	my($worksheet_name, $Sheet, $excel_object) = setup_EXCEL_object(shift);
-	
+
 	##read and process each row in the EXCEL file
 	my $usedRange = $Sheet->UsedRange()->{Value};
-			
+
 		shift(@$usedRange);
 
 		my $CurrentRow=2;
 
 		while (my $row=shift @$usedRange)
 		{
-			
+
 			($title, $authors, $advisors, $year, $filename) = @$row;
-			
+
 			$filename =~ s/.pdf//i;
 			my $fh=open_ouput_file($filename);
-			
+
 			mods_title($fh, $title);
 			mods_name_element_author($fh, $authors);
 			mods_name_element_advisor($fh, $advisors);
@@ -45,7 +45,7 @@ sub main
 			mods_access_condition($fh);
 			mods_extension($fh);
 			mods_record_info($fh);
-			
+
 			close_output_file ($fh);
 		};
 
@@ -69,18 +69,18 @@ if ($title =~ m/\&/i )
 	{$title =~ s/\&/\&amp;/g;};
 
 if ($title =~ m/\:/i )
-	{	
+	{
 	my ($title, $subtitle) = split (/:\s/, $title, 2);
 	my $nonsort;
-if ($title =~ m/^The (.*)/) 
-	{$nonsort = "The "; 
-	$title=$1} 
-elsif ($title =~ m/^A (.*)/) 
+if ($title =~ m/^The (.*)/)
+	{$nonsort = "The ";
+	$title=$1}
+elsif ($title =~ m/^A (.*)/)
 	{$nonsort = "A ";
-	$title=$1} 
-elsif ($title =~ m /^An (.*)/) 
+	$title=$1}
+elsif ($title =~ m /^An (.*)/)
 	{$nonsort = "An ";
-	$title=$1}; 
+	$title=$1};
 
 $fh->print("<mods:titleInfo usage=\"primary\">\n");
 
@@ -88,7 +88,7 @@ if ($nonsort) {$fh->print ("\t<mods:nonSort>$nonsort<\/mods:nonSort>\n")};
 
 $fh->print ("\t<mods:title>$title<\/mods:title>\n");
 
-if ($subtitle) 
+if ($subtitle)
 	{$fh->print ("\t<mods:subTitle>$subtitle<\/mods:subTitle>\n");}
 $fh->print("<\/mods:titleInfo>\n\n");
 
@@ -97,15 +97,15 @@ $fh->print("<\/mods:titleInfo>\n\n");
 else	{
 ##Deal with initial articles
 my $nonsort;
-if ($title =~ m/^The (.*)/) 
-	{$nonsort = "The "; 
-	$title=$1} 
-elsif ($title =~ m/^A (.*)/) 
+if ($title =~ m/^The (.*)/)
+	{$nonsort = "The ";
+	$title=$1}
+elsif ($title =~ m/^A (.*)/)
 	{$nonsort = "A ";
-	$title=$1} 
-elsif ($title =~ m /^An (.*)/) 
+	$title=$1}
+elsif ($title =~ m /^An (.*)/)
 	{$nonsort = "An ";
-	$title=$1}; 
+	$title=$1};
 
 $fh->print("<mods:titleInfo usage=\"primary\">\n");
 
@@ -113,7 +113,7 @@ if ($nonsort) {$fh->print ("\t<mods:nonSort>$nonsort<\/mods:nonSort>\n")};
 
 $fh->print ("\t<mods:title>$title<\/mods:title>\n");
 
-if ($subtitle) 
+if ($subtitle)
 	{$fh->print ("\t<mods:subTitle>$subtitle<\/mods:subTitle>\n");}
 $fh->print("<\/mods:titleInfo>\n\n");
 	}
@@ -123,7 +123,7 @@ $fh->print("<\/mods:titleInfo>\n\n");
 
 
 
-### See End of Document for MODS Author Element 
+### See End of Document for MODS Author Element
 
 
 
@@ -152,7 +152,7 @@ $fh->print("<mods:genre authority=\"marcgt\" type=\"work type\" usage=\"primary\
 
 sub mods_origin_info
 {
-	
+
 my ($fh, $year) = @_;
 
 $fh->print("<mods:originInfo>\n");
@@ -241,7 +241,7 @@ sub mods_record_info
 {
 my $fh = shift;
 
-$fh->print("<mods:recordInfo>\n");	
+$fh->print("<mods:recordInfo>\n");
 	$fh->print("\t<mods:recordContentSource authority=\"marcorg\">MChB<\/mods:recordContentSource>\n");
 	$fh->print("\t<mods:languageOfCataloging>\n\t\t<mods:languageTerm type=\"text\" authority=\"iso639-2b\">English<\/mods:languageTerm>\n\t\t<mods:languageTerm type=\"code\" authority=\"iso639-2b\">eng<\/mods:languageTerm>\n\t<\/mods:languageOfCataloging>\n");
 $fh->print("<\/mods:recordInfo>\n\n");
@@ -261,7 +261,7 @@ sub mods_name_element_author
 my $fh=shift;
 my $authors = shift;
 my $family;
-my $given; 
+my $given;
 
 
 my @authors = split(/\s*;\s*/, $authors);
@@ -276,7 +276,7 @@ my ($family_name, $given_name) = split(/\s*,\s*/, $display_form);
 
 $fh->print ("<mods:name type=\"personal\" usage=\"primary\">\n\t<mods:namePart type=\"family\">$family_name<\/mods:namePart>\n\t<mods:namePart type=\"given\">$given_name<\/mods:namePart>\n\t<mods:displayForm>$family_name, $given_name<\/mods:displayForm>\n\t<mods:role>\n\t\t<mods:roleTerm type=\"text\" authority=\"marcrelator\">Author<\/mods:roleTerm>\n\t\t<mods:roleTerm type=\"code\" authority=\"marcrelator\">aut<\/mods:roleTerm>\n\t<\/mods:role>\n<\/mods:name>\n\n");
 
-	} 
+	}
 
 }
 
@@ -287,7 +287,7 @@ sub mods_name_element_advisor
 my $fh=shift;
 my $advisors = shift;
 my $family;
-my $given; 
+my $given;
 
 
 my @advisors = split(/\s*;\s*/, $advisors);
@@ -302,7 +302,7 @@ my ($family_name, $given_name) = split(/\s*,\s*/, $display_form);
 
 $fh->print ("<mods:name type=\"personal\">\n\t<mods:namePart type=\"family\">$family_name<\/mods:namePart>\n\t<mods:namePart type=\"given\">$given_name<\/mods:namePart>\n\t<mods:displayForm>$family_name, $given_name<\/mods:displayForm>\n\t<mods:role>\n\t\t<mods:roleTerm type=\"text\" authority=\"marcrelator\">Thesis advisor<\/mods:roleTerm>\n\t\t<mods:roleTerm type=\"code\" authority=\"marcrelator\">ths<\/mods:roleTerm>\n\t<\/mods:role>\n<\/mods:name>\n\n");
 
-	} 
+	}
 
 }
 
@@ -316,13 +316,13 @@ sub setup_EXCEL_object {
 
 #Get the name of the excel workbook and worksheet you want to process
 print "\n\nEnter the name of the Excel file containing \nthe data you wish to convert to MODS: ";
-my $excelfile = <STDIN>; 
-chomp $excelfile; 
+my $excelfile = <STDIN>;
+chomp $excelfile;
 exit 0 if (!$excelfile);
 
 print "\n\nName of the worksheet containing the \ndata you wish to convert to MODS: ";
-my $worksheet_name = <STDIN>; 
-chomp $worksheet_name; 
+my $worksheet_name = <STDIN>;
+chomp $worksheet_name;
 exit 0 if (!$worksheet_name);
 
 my $dir = getcwd;
@@ -344,16 +344,16 @@ my $Excel = Win32::OLE->GetActiveObject('Excel.Application')
 
 #Turn off all the alert boxes, such as the SaveAs response "This file already exists", etc. using the DisplayAlerts property.
 
-$Excel->{DisplayAlerts}=0;   
+$Excel->{DisplayAlerts}=0;
 
-#Open an existing file to work with 
-                                                 
-my $book_object = $Excel->Workbooks->Open($excelfile);   
+#Open an existing file to work with
+
+my $book_object = $Excel->Workbooks->Open($excelfile);
 
 #Create a reference to a worksheet object and activate the sheet to give it focus so that actions taken on the workbook or application objects occur on this sheet unless otherwise specified.
 
 my $sheet_object = $book_object->Worksheets($worksheet_name);
-$sheet_object->Activate();  
+$sheet_object->Activate();
 
 return ($worksheet_name, $sheet_object, $Excel);
 }
@@ -370,7 +370,7 @@ $fh = IO::File->new($fh.'.xml', 'w')
 	or die "unable to open output file for writing: $!";
 binmode($fh, ':utf8');
 $fh->print("<?xml version='1.0' encoding='UTF-8' ?>\n");
-$fh->print("<mods:mods xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:mods=\"http://www.loc.gov/mods/v3\" xmlns:etdms=\"http://www.ndltd.org/standards/metadata/etdms/1.0/\" xsi:schemaLocation=\"http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd http://www.ndltd.org/standards/metadata/etdms/1.0/ http://www.ndltd.org/standards/metadata/etdms/1.0/etdms.xsd\">\n\n");
+$fh->print("<mods:mods xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:mods=\"http://www.loc.gov/mods/v3\" xmlns:etdms=\"http://www.ndltd.org/standards/metadata/etdms/1.0/\" xsi:schemaLocation=\"http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd http://www.ndltd.org/standards/metadata/etdms/1-0/ http://www.ndltd.org/standards/metadata/etdms/1-0/etdms.xsd\">\n\n");
 
 return($fh);
 
